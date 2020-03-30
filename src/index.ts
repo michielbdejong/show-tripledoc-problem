7f7778b525626b890ba7b95aace57249876b7e99
Michiel de Jong <michiel@unhosted.org>
2020-03-30 15:54:27 +0200

    Show build problem

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,14 @@
+import { createDocumentInContainer } from "tripledoc";
+
+async function test(): Promise<void> {
+  const doc = createDocumentInContainer(
+    "https://michielbdejong.inrupt.net/inboex/"
+  );
+  const sub = doc.addSubject("#hi");
+  sub.addString("https://example.com/#hello", "world");
+  await doc.save();
+}
+
 export function HelloWorld(): string {
   return "hello, world";
 }
